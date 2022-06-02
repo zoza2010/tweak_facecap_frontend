dd6ad74a272219f05f1fd3150268453f9c499afb
i.zuykov <i.zuykov@souzmult.ru>
2022-06-02 21:42:21 +0300

    disabled picker

--- widgets/facecap_tweak_widget.py
+++ widgets/facecap_tweak_widget.py
@@ -15,32 +15,32 @@ class FacecapTweak(QtWidgets.QWidget):
         """
         super(FacecapTweak, self).__init__(parent)
         self.setMouseTracking(True)
-        self.picker = Picker()
+        # self.picker = Picker()
         self.editor = EditorView()
-        self.editor.setVisible(True)
-        self.picker.setVisible(False)
+        # self.editor.setVisible(True)
+        # self.picker.setVisible(False)
         self.main_layout = QtWidgets.QVBoxLayout()
-        self.main_layout.addWidget(self.picker)
+        # self.main_layout.addWidget(self.picker)
 
         self.main_layout.addWidget(self.editor)
         self.setLayout(self.main_layout)
-        self.picker.area_clicked.connect(self.on_area_clicked)
+        # self.picker.area_clicked.connect(self.on_area_clicked)
         # self.editor.goBackPushButton.clicked.connect(self._switch_widgets)
 
     def setModel(self, model):
         self.editor.setModel(model)
 
-    def _switch_widgets(self):
-        self.picker.setVisible(not self.picker.isVisible())
-        self.editor.setVisible(not self.editor.isVisible())
+    # def _switch_widgets(self):
+    #     self.picker.setVisible(not self.picker.isVisible())
+    #     self.editor.setVisible(not self.editor.isVisible())
 
-    def on_area_clicked(self, clr):
-        if clr == Picker.kTopSelected:
-            self._switch_widgets()
-        elif clr == Picker.kMidSelected:
-            self._switch_widgets()
-        elif clr == Picker.kBottomSelected:
-            self._switch_widgets()
-
-    def mouseMoveEvent(self, event: QtGui.QMouseEvent) -> None:
-        self.picker.mouseMoveEvent(event)
+    # def on_area_clicked(self, clr):
+    #     if clr == Picker.kTopSelected:
+    #         self._switch_widgets()
+    #     elif clr == Picker.kMidSelected:
+    #         self._switch_widgets()
+    #     elif clr == Picker.kBottomSelected:
+    #         self._switch_widgets()
+    #
+    # def mouseMoveEvent(self, event: QtGui.QMouseEvent) -> None:
+    #     self.picker.mouseMoveEvent(event)
