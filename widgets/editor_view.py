7ea5b460923093d3915b694dfd02066fb4bf1f12
i.zuykov <vania.zuickov@yandex.ru>
2022-07-17 22:47:56 +0300

    slider frame range fits value

--- widgets/editor_view.py
+++ widgets/editor_view.py
@@ -3,6 +3,7 @@ from PySide6 import QtGui
 from PySide6 import QtCore
 from PySide6 import QtWidgets
 from .ui.editor_view import Ui_Form
+import six
 
 
 from pynput.keyboard import Key, Controller
@@ -17,6 +18,18 @@ class _ControlsDataView(QtWidgets.QTableView):
         return result
 
 
+# this delegate resizes slider boundaries to fit input value
+class SliderItemDelegate(QtWidgets.QItemDelegate):
+    def setEditorData(self, widget: QtWidgets.QWidget, index: QtCore.QModelIndex):
+        if isinstance(widget, QtWidgets.QSlider):
+            value = int(index.data(QtCore.Qt.DisplayRole))
+            if value < widget.minimum():
+                widget.setMinimum(value)
+            elif value > widget.maximum():
+                widget.setMaximum(value)
+        return super(SliderItemDelegate, self).setEditorData(widget, index)
+
+
 class EditorView(QtWidgets.QWidget, Ui_Form):
     def __init__(self, parent=None):
         super(EditorView, self).__init__(parent)
@@ -27,7 +40,7 @@ class EditorView(QtWidgets.QWidget, Ui_Form):
         self._mapper_start_column = 1
         self._mapper_row = 0
         self._mapper = QtWidgets.QDataWidgetMapper(self)
-
+        self._mapper.setItemDelegate(SliderItemDelegate())
         # setup search filter
         self._filter_proxy_model = QtCore.QSortFilterProxyModel()
         self._filter_proxy_model.setFilterCaseSensitivity(QtCore.Qt.CaseInsensitive)
@@ -44,7 +57,22 @@ class EditorView(QtWidgets.QWidget, Ui_Form):
         self.offsetSlider.valueChanged.connect(self.force_press_enter)
         self.scaleSlider.valueChanged.connect(self.force_press_enter)
 
+        self._sliders = [
+            self.clampMaxSlider,
+            self.clampMinSlider,
+            self.offsetSlider,
+            self.scaleSlider,
+        ]
+
+        self._sliders_default_ranges = list(
+            six.moves.map(lambda x: (x.minimum(), x.maximum()), self._sliders)
+        )
+
     def force_press_enter(self):
+        """
+        this force-updates all views by emulating "Enter" press key
+        :return:
+        """
         keyboard = Controller()
         keyboard.press(Key.enter)
         keyboard.release(Key.enter)
@@ -83,19 +111,20 @@ class EditorView(QtWidgets.QWidget, Ui_Form):
         # self._line_edits_mapper.toFirst()
         self._mapper.toFirst()
 
-    def _on_layout_changed(self):
+    def _on_all_data_changed(self):
+        # set sliders to default values
+        for values, slider in zip(self._sliders_default_ranges, self._sliders):
+            slider.setRange(*values)
         self._attach_mappings()
 
     def model(self):
         return self._model
 
     def setModel(self, model):
-
         self._filter_proxy_model.setSourceModel(model)
         # TODO temp solution
         self._model = self._filter_proxy_model
-        model.all_data_changed.connect(self._on_layout_changed)
-
+        self._model.sourceModel().all_data_changed.connect(self._on_all_data_changed)
         self.listView.setModel(self._model)
         # self._line_edits_mapper.setModel(self._model)
         # self._clamp_min_slider_mapper.setModel(self._model)
