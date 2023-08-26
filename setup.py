0c58ed327cfffbfb1232514f707610eda303e68b
i.zuykov <vania.zuickov@gmail.com>
2023-08-26 21:20:17 +0300

    perf: moved to subfolder

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 __version__ = "0.1.0"
 
 setup(
-   name="my-package",
-   version=__version__,
-   # And so on...
+    name="my-package",
+    version=__version__,
+    # And so on...
 )
